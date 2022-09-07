b856bf6b686053d920ba9fdec411a54ba0af094b
Utku Sarioglu <32932503+utkusarioglu@users.noreply.github.com>
2022-09-07 07:08:45 +0000

    Chore(script): Alter ADB script
    - Add `.env.HOST_IP` check to the script. The script now fails
      if the environment variable is not available.
    - Add an echo statement before the connection starts, this is
      done to let the user know that something is happening, and to
      inform the user of the target of connection.

--- scripts/adb-connect.sh
+++ scripts/adb-connect.sh
@@ -2,4 +2,14 @@
 
 source .env
 
-adb connect "$HOST_IP:58526"
+port=58526
+
+if [ -z "$HOST_IP" ];
+then
+  echo "Error: .env.HOST_IP has to be defined for this script to work"
+  exit 1
+fi
+
+target="$HOST_IP:$port"
+echo "Trying to connect to: $target"
+adb connect $target
