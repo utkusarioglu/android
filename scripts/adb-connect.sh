cb4690075ade96e65c95ec7fea6c3928fe8a971e
Utku Sarioglu <utkusarioglu@hotmail.com>
2022-10-18 10:22:45 +0000

    Chore(adb): Alter tasks, host, script for adb
    - Add `extra_hosts` in `docker-compose.dev.yml:6` to use `host-gateway`
      for connecting to the android vm.
    - Alter adb script to use the new host gateway url.
    - Alter the `close` behavior of ADB tasks in vscode `tasks.json`.
    - Remove environment variable that holds host ip. This is no longer
      needed.

--- scripts/adb-connect.sh
+++ scripts/adb-connect.sh
@@ -4,12 +4,6 @@ source .env
 
 port=58526
 
-if [ -z "$HOST_IP" ];
-then
-  echo "Error: .env.HOST_IP has to be defined for this script to work"
-  exit 1
-fi
-
-target="$HOST_IP:$port"
+target="android-host:$port"
 echo "Trying to connect to: $target"
 adb connect $target
